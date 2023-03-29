63271cb9f6b7d564c70fae3cc7f7a3098a07663c
root <root@beaglebone.localdomain>
2023-03-29 00:34:38 +0000

    Working IMU_Code that gives euler, gravity, acceleration, gyro, and quaternion.

--- IMU_Code.py
+++ IMU_Code.py
@@ -1,3 +1,4 @@
+
 #IMU
 
 import adafruit_bno055
@@ -7,5 +8,13 @@ i2c = board.I2C()
 
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
+count = 0
 
-print(sensor.gravity)
+while(count<10):
+	print('Euler: {}'.format(sensor.euler))
+	print("Gravity: {}".format(sensor.gravity))
+	print("Acceleration: {}".format(sensor.acceleration))
+	print("Gyroscope: {}".format(sensor.gyro))
+	print('Quaternion: {}'.format(sensor.quaternion))
+
+	count += 1
