d8acd6d413012b29819b0ecf873887ea69dc52be
root <root@beaglebone.localdomain>
2023-03-29 00:51:20 +0000

    Update to IMU, made it cleaner and added linear acceleration

--- IMU_Code.py
+++ IMU_Code.py
@@ -1,20 +1,19 @@
-
 #IMU
 
 import adafruit_bno055
 import board
+import time
 
 i2c = board.I2C()
 
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
-count = 0
 
-while(count<10):
-	print('Euler: {}'.format(sensor.euler))
+while(1):
+	print("Euler: {}".format(sensor.euler))
 	print("Gravity: {}".format(sensor.gravity))
 	print("Acceleration: {}".format(sensor.acceleration))
 	print("Gyroscope: {}".format(sensor.gyro))
-	print('Quaternion: {}'.format(sensor.quaternion))
-
-	count += 1
+	print("Quaternion: {}".format(sensor.quaternion))
+	print("Linear Acceleration: {}".format(sensor.linear_acceleration))
+	time.sleep(0.25) #only give reading every quarter second
