7d7d5d3c56f09259f817086e24628233d6d31cd8
root <root@beaglebone.localdomain>
2023-04-01 23:45:38 +0000

    Fixed PWM code, now successfully starts up ESC and runs motor

--- PWM.py
+++ PWM.py
@@ -2,15 +2,27 @@ import Adafruit_BBIO.PWM as PWM
 import time
 
 motorPin = "P9_14"
-PWM.start(motorPin,100,500) #50Hz PWM frequency, will want to ramp this up later - ESCs can go up to 500Hz
+PWM.start(motorPin,50,500) #50Hz PWM frequency, will want to ramp this up later - ESCs can go up to 500Hz
 
-duty_cycle = 55
+duty_cycle = 50
+
+print("plug in motor now")
+
+time.sleep(5)
 
 PWM.set_duty_cycle(motorPin, duty_cycle)
 
-while(duty_cycle > 0):
+time.sleep(2)
 
-	time.sleep(0.1)
+while(1):
+
+	duty_cycle_input = input("Enter desired thrust level (50 is 0%, 100 is 100%: ")
+	duty_cycle = int(duty_cycle_input)
+	#print(duty_cycle)
+	#permission = input("Proceed? (y/n): ")
+
+	#if permission == "y":
 	PWM.set_duty_cycle(motorPin, duty_cycle)
-	duty_cycle = duty_cycle - 5;
+	#else:
+		#break
 
