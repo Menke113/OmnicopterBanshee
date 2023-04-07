cd220e99572c8674a9d814c6c045e98a50d43949
root <root@beaglebone.localdomain>
2023-04-07 18:17:23 +0000

    work

--- IMU_Code.py
+++ IMU_Code.py
@@ -9,11 +9,27 @@ i2c = board.I2C()
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 
-while(1):
-	print("Euler: {}".format(sensor.euler))
-	print("Gravity: {}".format(sensor.gravity))
-	print("Acceleration: {}".format(sensor.acceleration))
-	print("Gyroscope: {}".format(sensor.gyro))
-	print("Quaternion: {}".format(sensor.quaternion))
-	print("Linear Acceleration: {}".format(sensor.linear_acceleration))
-	time.sleep(0.25) #only give reading every quarter second
+
+
+#while(count<15):
+#	quaternionData = sensor.quaternion
+#	eulerData = sensor.euler
+#	gravityData = sensor.gravity
+#	print("Euler: {}".format(eulerData))
+#	print("Gravity: {}".format(gravityData))
+#	print("Acceleration: {}".format(sensor.acceleration))
+#	print("Gyroscope: {}".format(sensor.gyro))
+#	print("Quaternion: {}".format(quaternionData))
+#	print("Linear Acceleration: {}".format(sensor.linear_acceleration))
+#	time.sleep(0.25) #only give reading every quarter second
+#	count = count + 1
+
+
+#def set_e_data():
+#	eulerData = sensor.euler
+#i = 1
+#while(i<10):
+
+#	quaternionData = sensor.quaternion
+#	eulerData = sensor.euler
+#	i =i +1
--- test.py
+++ test.py
@@ -1,1 +1,22 @@
+import IMU_Code
+import time
+
 print("Hello World")
+
+#eulerData = IMU_Code.eulerData
+
+c =3
+while(c<10):
+	print(IMU_Code.sensor.quaternion)
+	#IMU_Code.set_e_data()
+	print(IMU_Code.sensor.euler)
+#	print("Euler: {}".format(eulerData))
+#	time.sleep(0.25) #only give reading every quarter second
+	c = c + 1
+print(IMU_Code.sensor.quaternion)
+        #IMU_Code.set_e_data()
+print(IMU_Code.sensor.euler)
+print(IMU_Code.sensor.quaternion)
+        #IMU_Code.set_e_data()
+print(IMU_Code.sensor.euler)
+
