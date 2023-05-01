Fixed accel_mods sign and mde sure duty cycles don't go out of range

control_loop.py
@@ -28,9 +28,9 @@ def control_loop():
     motorPin7 = "P8_19" # good
     motorPin8 = "P9_14" # doubled with P8_36
 
-    max_yaw = 100 # max yaw rate, rad/s
-    max_roll = 100 # max roll rate, rad/s
-    max_pitch = 100 # max pitch rate, rad/s
+    max_yaw = 0.628 # max yaw rate, rad/s
+    max_roll = 0.628 # max roll rate, rad/s
+    max_pitch = 0.628 # max pitch rate, rad/s
 
     des_x = 0
     des_y = 0
@@ -87,14 +87,18 @@ def control_loop():
             PWM.set_duty_cycle(motorPin6, duty_cycle)
             PWM.set_duty_cycle(motorPin7, duty_cycle)
             PWM.set_duty_cycle(motorPin8, duty_cycle)
-            break
+
             arm_loop()
 
         Rx_chan = asyncio.run(get_frame())
-        s0 = (Rx_chan[0]-173)/(1810-173) * 2 - 1 # yaw input, 0-1
-        s1 = (Rx_chan[1]-173)/(1810-173) * 2 - 1 # throttle input, 0-1
-        s2 = (Rx_chan[2]-173)/(1810-173) * 2 - 1 # pitch input, 0-1
-        s3 = (Rx_chan[3]-173)/(1810-173) * 2 - 1 # roll input, 0-1
+        s0 = (Rx_chan[0]-173)/(1810-173) * 2 - 1 # yaw input, -1 to 1
+        s1 = (Rx_chan[1]-173)/(1810-173) # throttle input, -1 to 1
+        s2 = (Rx_chan[2]-173)/(1810-173) * 2 - 1 # pitch input, -1 to 1
+        s3 = (Rx_chan[3]-173)/(1810-173) * 2 - 1 # roll input, -1 to 1
+
+        max_thrust_setting = s1 * 25
+
+        print
 
         des_yaw = s0 * max_yaw
         des_roll = s3 * max_roll
@@ -104,7 +108,7 @@ def control_loop():
         # set desired x velocity:
         if Rx_chan[6] < 250 and Rx_chan[5] != prev_switch5 and i != 0:
 
-            if Rx_chan[5] < 250 and Rx_chan[5]:
+            if Rx_chan[5] < 250:
                 print('Set des_x to 100')
                 des_x = max_rate_x # 100 mm/s
 
@@ -123,7 +127,7 @@ def control_loop():
                 print('Set des_y to 100')
                 des_y = max_rate_y # 100 mm/s
 
-            if Rx_chan[5] < 1800 and Rx_chan[4] > 250:
+            if Rx_chan[5] < 1800 and Rx_chan[5] > 250:
                 print('Set des_y to 0')
                 des_y = 0 # 100 mm/s
 
@@ -187,38 +191,63 @@ def control_loop():
         prev_error_pitch = PID_pitch[1]
         error_sum_pitch = PID_pitch[2]
 
-        accel_mods = [xlinc, ylinc, zlinc, yawc, rollc, pitchc]
+
+        accel_mods = [xlinc * np.sign(des_x - linvel[0]), ylinc * np.sign(des_y - linvel[1]), zlinc * np.sign(des_z - linvel[2]), 
+        yawc * np.sign(des_yaw - psi), rollc * np.sign(des_roll - phi), pitchc * np.sign(des_pitch - theta)]
+
+        print('accel_mods is: ')
+        print(accel_mods)
+
         des_state = [des_x, des_y, des_z, des_yaw, des_roll, des_pitch]
 
         # now that we have controller outputs, call dynamics function with the inputted desired accelerations scaled by the controller outputs:
         # print("here")
-        # T = asyncio.run(get_T(accel_mods, des_state))
+        T = asyncio.run(get_T(accel_mods, des_state))
 
-        T = asyncio.run(get_T())
+        # T = asyncio.run(get_T())
 
-        # print(T)
+        print('desired forces and moments: ')
+        print('T before adjustment to N: ')
+
+        print(T)
+
+        T[:3] = T[:3] * (np.array([0.009806552] * 3))
+
+        print(T)
 
         # call optimization to get desired angular velocities of motors
 
         # thrusts = optim_thrust(T, max_thrusts, thrusts)
 
+
         thrusts = get_thrusts(T)
+        print('desired motor thrusts from get_thrust() are:')
+        print(thrusts)
         # print('thrusts: ')
         # print(thrusts)
         # print('max_thrusts: ')
         # print(max_thrusts)
         throttles = thrusts/max_thrusts
         duty_cycles = throttles * 25 + 75
-        for dc in duty_cycles:
-            if dc < 50:
-                dc = 50
-            if dc > 100:
-                dc = 100
 
-        # print('throttle levels: ')
-        # print(throttles)
-        # print('duty_cycles: ')
+        # print('duty_cycles before loop: ')
         # print(duty_cycles)
+
+
+        i = 0
+        for dc in duty_cycles:
+            if dc < (75 - max_thrust_setting):
+                dc = 75 - max_thrust_setting
+            if dc > (75 + max_thrust_setting):
+                dc = 75 + max_thrust_setting
+
+            duty_cycles[i] = dc
+            i += 1
+
+        print('throttle levels: ')
+        print(throttles)
+        print('duty_cycles after loop: ')
+        print(duty_cycles)
 
         # now back out throttle level from desired omegas of the motors and command this throttle level to the motors
 

dynamicsModel.py
@@ -7,80 +7,8 @@ import time
 import dataParser
 from dataParser import *
 import asyncio
-"""
-#Constant
-Ixx = 22036654.9
-Iyy = 35990963.19
-Izz = 15507494.32
-I = np.array([[Ixx, 0, 0], [0, Iyy, 0],[0, 0, Izz]])
-#from IMU
-phi = get_euler()[0]
-theta = get_euler()[1]
-psi = get_euler()[2]
-angle_pos = np.array([[phi],[theta],[psi]])
-#Calculate
-time_old = -1
-angular_velocity_old = np.array([[-1000000],[-1000000],[-1000000]])
-def get_angular_accel(time_old, angular_velocity_old):
-        if time_old == -1:
-                time_old = np.transpose(get_gyro())
-        if (angular_velocity_old <= -1000000).all():
-                angular_velocity_old = np.transpose(get_gyro())
-        time_new = time.time()
-        angular_velocity_new = np.transpose(get_gyro())
-        accel = np.subtract(angular_velocity_new,angular_velocity_old)/(time_new-time_old)
-        angular_velocity_old = angular_velocity_new
-        time_old = time_new
-        phi_ddot = accel[0]
-        theta_ddot = accel[1]
-        psi_ddot = accel[2]
-        angle_accel = np.array([[phi_ddot],[theta_ddot],[psi_ddot]])
-        return [angle_accel, time_old, angular_velocity_old]
-[a,t,o] = get_angular_accel(time_old, angular_velocity_old)
-[a,t,o] = get_angular_accel(t, o)
-print(a)
-print(t)
-phi_ddot = a[0]
-theta_ddot = a[1]
-psi_ddot = a[2]
-angle_accel = a
-#I #moments of inertia A #angular acceleration T #Translational
-#from the transmiter
-async def get_angle_vel():
-        frame_data = await get_frame()
-        phi_dot = frame_data[3]     #roll
-        theta_dot = frame_data[2]   #pitch
-        psi_dot = frame_data[0]     #yaw
-        angle_vel = np.array([[phi_dot],[theta_dot],[psi_dot]])
-        return angle_vel
-r = np.array([[1,0,-m.sin(theta)],[0,m.cos(phi),m.cos(theta)*m.sin(phi)],[0,-m.sin(phi),m.cos(theta)*m.cos(phi)]]) #small rotation matrix
-w = np.multiply(r,asyncio.run(get_angle_vel()))
-w_dot = np.multiply(r,angle_accel)
-#receiver input for angular velocities
-Tphi = I[0]*w_dot[0] +(I[1]-I[2])*w[1]*w[2]
-Ttheta = I[1]*w_dot[1] +(I[2]-I[0])*w[0]*w[2]
-Tpsi = I[2]*w_dot[2] +(I[0]-I[1])*w[0]*w[1]
-### TRANSLATION
-trans_accel = get_acceleration()
-mass = 1.875 #kg
-kdx = 0.24      #.18-.24
-kdy = 0.24              # TO BE DISCOVERED
-kdz = 0.24
-direction = 0
-x_dot = 0
-y_dot = 0
-z_dot = 0
-async def get_vel():
-        frame_data = await get_frame()
-        phi_dot = frame_data[3]     #roll
-        theta_dot = frame_data[2]   #pitch
-        psi_dot = frame_data[0]     #yaw
-        vel = np.array([[phi_dot],[theta_dot],[psi_dot]])
-        return vel
-Tx = trans_accel[0]*mass-kdx*x_dot
-Ty = trans_accel[1]*mass-kdy*y_dot
-Tz = trans_accel[2]*mass-kdz*z_dot
-"""
+
+
 ### CONSTANTS
 mass = 1.875 #kg
 
@@ -101,71 +29,32 @@ y_dot = 0
 z_dot = 0
 
 
-time_old = -1
-angular_velocity_old = np.array([[-1000000],[-1000000],[-1000000]])
 
 
-async def get_T():
+
+async def get_T(accel_mods,des_state):
 	frame_data = await get_frame()
-#	print(frame_data)
-#	time.sleep(0.1)
+
 	#Rotation
 	phi = get_euler()[0]
 	theta = get_euler()[1]
 	psi = get_euler()[2]
 
 	angle_pos = np.array([[phi],[theta],[psi]])
+	# print('angle_pos')
+	# print(angle_pos)
 
-	time_old = -1
-	angular_velocity_old = np.array([[-1000000],[-1000000],[-1000000]])
+	phi_ddot = accel_mods[4]
+	theta_ddot = accel_mods[5]
+	psi_ddot = accel_mods[3]
+	angle_accel = [phi_ddot,theta_ddot,psi_ddot]
+	# print("angle_accel")
+	# print(angle_accel)
 
-	def get_angular_accel(time_old, angular_velocity_old):
-		if time_old == -1:
-			time_old = np.transpose(get_gyro())
-		if (angular_velocity_old <= -1000000).all():
-			angular_velocity_old = np.transpose(get_gyro())
+	angle_vel = [des_state[1],des_state[2],des_state[0]]
 
-		time_new = time.time()
-		angular_velocity_new = np.transpose(get_gyro())
-		accel = np.subtract(angular_velocity_new,angular_velocity_old)/(time_new-time_old)
-
-		angular_velocity_old = angular_velocity_new
-		time_old = time_new
-		phi_ddot = accel[0]
-		theta_ddot = accel[1]
-		psi_ddot = accel[2]
-
-		angle_accel = [phi_ddot,theta_ddot,psi_ddot]
-
-		return [angle_accel, time_old, angular_velocity_old]
-
-	[a,t,o] = get_angular_accel(time_old, angular_velocity_old)
-	[a,t,o] = get_angular_accel(t, o)
-
-	phi_ddot = a[0]
-	theta_ddot = a[1]
-	psi_ddot = a[2]
-
-	angle_accel = a
-#	print("angle accel")
-#	print(a)
-
-#	while 1:
-#		[a,t,o] = get_angular_accel(t, o)
-#		time.sleep(1)
-#		print(a)
-	#[993, 997, 991]
-
-
-	phi_dot = (frame_data[3]-173)/(1810-173)-0.5     #roll
-	theta_dot = (frame_data[2]-173)/(1810-173)-0.5    #pitch
-	psi_dot = (frame_data[0]-173)/(1810-173)-0.5      #yaw
-
-	angle_vel = [phi_dot,theta_dot,psi_dot]
-#	print("angleVel")
-#	print(angle_vel)
-
-
+	# print("angle_vel")
+	# print(angle_vel)
 
 
 	w = [0,0,0]
@@ -174,19 +63,19 @@ async def get_T():
 		w[x] = angle_vel[0]*r[x][0]+angle_vel[1]*r[x][1]+angle_vel[2]*r[x][2]
 #	print("my w")
 #	print(w)
-	#w = np.multiply(r,angle_vel)
+	#w = np.multiply(r,angle_accel)
 	w_dot = [0,0,0]
 	for x in range(0,3):
 		w_dot[x] = angle_accel[0]*r[x][0]+angle_accel[1]*r[x][1]+angle_accel[2]*r[x][2]
 
 #	w_dot = np.multiply(r,angle_accel)
-#	print("r")
-#	print(r)
-#	print("w")
-#	print(w)
-#	print("w_dot")
-#	print(w_dot)
-#	return [r,w]
+	# print("r")
+	# print(r)
+	# print("w")
+	# print(w)
+	# print("w_dot")
+	# print(w_dot)
+
 
 #	print(r)
 	Tphi = Ixx*w_dot[0] +(Iyy-Izz)*w[1]*w[2]
@@ -200,59 +89,35 @@ async def get_T():
 #	print(w[1])
 #	print(Ttheta)
 #	print(Tpsi)
-#	print([Tphi,Ttheta,Tpsi])
+	# print("[Tphi,Ttheta,Tpsi]")
+	# print([Tphi,Ttheta,Tpsi])
 
 	#TRANSLATION
-	# 5 is direction
-	# 6 is which axis
-	trans_accel = get_acceleration()
 #	thrust = 0.1 #100mm/s #(frame_data[1]-173)/(1810-173)
 
-
-	if frame_data[5] < 250:
-		direction = 1
-#		print("high")
-	elif frame_data[5] > 1800:
-		direction = -1
-#		print("low")
-	else:
-		direction = 0
-#		print("hover")
-
-
-	if frame_data[6] < 250:
-		# z axis
-		z_dot = direction * thrust
-#		print("z")
-		x_dot = 0
-		y_dot = 0
-	elif frame_data[6] > 1800:
-		# y axis
-		y_dot = direction * thrust
-#		print("y")
-		x_dot = 0
-		z_dot = 0
-	else:
-		# x axis
-		x_dot = direction * thrust
-#		print("x")
-		y_dot = 0
-		z_dot = 0
 
 #	kdx = 0.24	#.18-.24
 #	kdy = 0.24		# TO BE DISCOVERED
 #	kdz = 0.24
 #	print("here")
-#	y_dot = 0
-#	z_dot = 0
-#	x_dot = 0
-	Tx = trans_accel[0]*mass-kdx*x_dot
-	Ty = trans_accel[1]*mass-kdy*y_dot
-	Tz = trans_accel[2]*mass-kdz*z_dot
-#	print([Tx,Ty,Tz])
+	y_dot = des_state[1]
+	z_dot = des_state[2]
+	x_dot = des_state[0]
+	# print("x_dot")
+	# print(x_dot)
+	# print("y_dot")
+	# print(y_dot)
+	# print("z_dot")
+	# print(z_dot)
+	g = get_gravity()
+	Tx = (accel_mods[0]+g[0])*mass-kdx*x_dot
+	Ty = (accel_mods[1]+g[1])*mass-kdy*y_dot
+	Tz = (accel_mods[2]+g[2])*mass-kdz*z_dot
+	# print("[Tx,Ty,Tz]")
+	# print([Tx,Ty,Tz])
 #	print(Tx[0])
 #	print("before return")
-	T = [Tx[0],Ty[0],Tz[0],Tphi,Ttheta,Tpsi]   #There is some weird formating that the [0] get around
+	T = [Tx,Ty,Tz,Tphi,Ttheta,Tpsi]   #There is some weird formating that the [0] get around
 	return T
 
 #x = asyncio.run(get_T())
@@ -263,40 +128,6 @@ async def get_T():
 #task = loop.create_task(get_T())
 #remaining_work_not_depends_on_get_T()
 #loop.run_until_complete(task)
-"""
-max1 = 0
-max2 = 0
-max3 = 0
-min1 = 0
-min2 = 0
-min3 = 0
-while 1:
-	time.sleep(0.1)
-	x = asyncio.run(get_T())
-	print(x)
-	if x[3] > max1:
-		max1 = x[3]
-#	print(max1)
-#	print(min1)
-	if x[4] > max2:
-		max2 = x[4]
-#	print(max2)
-#	print(min2)
-	if x[5] > max3:
-		max3 = x[5]
-#	print(max3)
-#	print(min3)
-	if x[3] < min1:
-		min1 = x[3]
-	if x[4] < min2:
-		min2 = x[4]
-	if x[5] < min3:
-		min3 = x[5]
-"""
 
-#print(z)
-#print(a)
-#print(b)
-#print(c)
 #print([x,y,z,a,b,c])
-print("end")
+#print("end")